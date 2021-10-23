96d389979c39418b046dbc2b1906d16bb4e420c4
Phillip Cannon <phizzcannon@gmail.com>
2021-10-23 21:51:15 +0100

    Switch deployment mode

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const winapi = require('winapi-bindings');
-const { fs, log, util } = require('vortex-api');
+const { fs, util } = require('vortex-api');
 
 // game variables
 const GAME_ID = 'theriftbreaker';
@@ -8,42 +8,45 @@ const GAME_NAME = 'The Riftbreaker';
 const GAME_EXECUTABLE = 'bin/riftbreaker_win_release.exe';
 
 // store variables
-const STEAMAPP_ID = '780310';
-const GOGAPP_ID = '2147483111';
 const EPICAPP_ID = '5bf44beb2a1f437696f637071203be7c';
+const GOGAPP_ID = '2147483111';
+const STEAMAPP_ID = '780310';
 
+// set this to true to deploy mods as zip files to the Packs folder
+// this was how the extension originally deployed mods, now disabled by default.
+const DEPLOY_PACKS = false;
+
+/**
+ * Entry point
+ */
 function main(context) {
   context.registerGame({
     id: GAME_ID,
     name: GAME_NAME,
     mergeMods: true,
     queryPath: findGame,
-    queryModPath: () => 'packs',
+    queryModPath: () => (DEPLOY_PACKS ? 'packs' : 'data'),
     logo: 'gameart.jpg',
     executable: () => GAME_EXECUTABLE,
     requiredFiles: [GAME_EXECUTABLE],
     supportedTools: [],
     setup: prepareForModding,
     environment: {
-      SteamAPPId: STEAMAPP_ID,
+      SteamAPPId: STEAMAPP_ID
     },
     details: {
-      steamAppId: STEAMAPP_ID,
-      gogAppId: GOGAPP_ID,
       epicAppId: EPICAPP_ID,
+      gogAppId: GOGAPP_ID,
+      steamAppId: STEAMAPP_ID
     },
   });
-
-  context.registerInstaller(
-    'theriftbreaker-mod',
-    25,
-    testSupportedContent,
-    installContent
-  );
 
   return true;
 }
 
+/**
+ * Find the game installation location.
+ */
 function findGame() {
   try {
     const instPath = winapi.RegGetValue(
@@ -56,12 +59,17 @@ function findGame() {
     }
     return Promise.resolve(instPath.value);
   } catch (err) {
-    return util.GameStoreHelper.findByAppId([STEAMAPP_ID, GOGAPP_ID, EPICAPP_ID]).then(
-      (game) => game.gamePath
-    );
+    return util.GameStoreHelper.findByAppId([
+      STEAMAPP_ID,
+      GOGAPP_ID,
+      EPICAPP_ID
+    ]).then((game) => game.gamePath);
   }
 }
 
+/**
+ * Deploys keeps mods in a zip file and deploys them to the Packs folder.
+ */
 async function installContent(files, destinationPath) {
   const szip = new util.SevenZip();
   const archiveName = path.basename(destinationPath, '.installing') + '.zip';
@@ -78,22 +86,32 @@ async function installContent(files, destinationPath) {
     {
       type: 'copy',
       source: archiveName,
-      destination: archiveName,
+      destination: archiveName
     },
   ];
 
   return Promise.resolve({ instructions });
 }
 
+/**
+ * Verify if a mod is compatible with the game.
+ */
 function testSupportedContent(_, gameId) {
   return Promise.resolve({
     supported: gameId === GAME_ID,
-    requiredFiles: [],
+    requiredFiles: []
   });
 }
 
+/**
+ * Ensure the game is ready to be modified.
+ */
 function prepareForModding(discovery) {
-  return fs.ensureDirWritableAsync(discovery.path);
+  if (DEPLOY_PACKS) {
+    return fs.ensureDirWritableAsync(path.join(discovery.path, 'packs'));
+  }
+
+  return fs.ensureDirWritableAsync(path.join(discovery.path, 'data'));
 }
 
 module.exports = {
