f4cc4182eb4ca058fa7dd113ffce74e783082b26
Phillip Cannon <phizzcannon@gmail.com>
2021-11-03 16:29:59 +0000

    Add winstore support

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { fs, util } = require('vortex-api');
 // game variables
 const GAME_ID = 'theriftbreaker';
 const GAME_NAME = 'The Riftbreaker';
-const GAME_EXECUTABLE = 'bin/riftbreaker_win_release.exe';
 
 // store variables
 const EPICAPP_ID = '5bf44beb2a1f437696f637071203be7c';
@@ -23,8 +22,19 @@ function main(context) {
     queryPath: findGame,
     queryModPath: () => 'data',
     logo: 'gameart.jpg',
-    executable: () => GAME_EXECUTABLE,
-    requiredFiles: [GAME_EXECUTABLE],
+    executable: (discoveryPath) => {
+      const winPath = 'bin/riftbreaker_win_release.exe';
+      const winStorePath = 'bin/riftbreaker_winstore_release.exe';
+
+      if (
+        discoveryPath !== undefined &&
+        getPathExists(path.join(discoveryPath, winStorePath))
+      )
+        return winStorePath;
+
+      return winPath;
+    },
+    requiredFiles: ['packs/00_win_data.zip'],
     supportedTools: [],
     setup: prepareForModding,
     environment: {
@@ -34,10 +44,22 @@ function main(context) {
       epicAppId: EPICAPP_ID,
       gogAppId: GOGAPP_ID,
       steamAppId: STEAMAPP_ID
-    },
+    }
   });
 
   return true;
+}
+
+/**
+ * Gets whether the specified path exists.
+ */
+function getPathExists(path) {
+  try {
+    fs.statSync(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
 
 /**
@@ -71,5 +93,5 @@ function prepareForModding(discovery) {
 }
 
 module.exports = {
-  default: main,
+  default: main
 };
